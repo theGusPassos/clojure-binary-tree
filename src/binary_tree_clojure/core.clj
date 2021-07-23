65d39b6da414f86dda02cd5f1874060141bc6f3f
theguspassos <theguspassos@gmail.com>
2021-07-22 22:29:11 -0300

    finish tree

--- src/binary_tree_clojure/core.clj
+++ src/binary_tree_clojure/core.clj
@@ -7,20 +7,17 @@
 (defn is-smaller? [new-node node]
   (< (:value new-node) (:value node)))
 
-(defn is-same? [new-node node]
-  (= (:value new-node) (:value node)))
-
 (declare add-node)
 
-(defn add-right-node [node new-node]
-  (assoc node :right (add-node (:right node) new-node)))
+(defn add-node-in [node side new-node]
+  (assoc node side (add-node (side node) new-node)))
 
 (defn add-node [node new-node]
   (cond
     (not (some? node))          new-node
-    (is-greater? new-node node) (add-right-node node new-node)
-    (is-smaller? new-node node) {:smaller 1}
-    :else {:else 1}))
+    (is-greater? new-node node) (add-node-in node :right new-node)
+    (is-smaller? new-node node) (add-node-in node :left new-node)
+    :else node))
 
 (defn create-node [value]
   {:value value})
@@ -30,19 +27,15 @@
   ([root]
    (let [node-value (Integer. (read-line))
          new-root   (add-node root (create-node node-value))]
-     (println "new root" new-root)
+     (println "new root\n" new-root "\n\n")
      (add-node-from-user-input new-root))))
 
 (defn -main
-  [& args]
+  [& _]
   (println "🌳 binary tree 🌳"
            "\n\n"
            "enter a non numeric value to exit")
   
   (try
     (add-node-from-user-input)
-    (catch Exception e (println "exiting..." e))))
-
-(-main)
-
-(Integer. (read-line))
+    (catch Exception _ (println "exiting..."))))
